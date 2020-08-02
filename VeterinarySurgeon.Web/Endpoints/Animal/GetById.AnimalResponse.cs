9818f3d7baf625a11140d5124373915bebf13457
David santana <dvdsantana@gmail.com>
2020-08-02 12:28:52 +0200

    Animal endpoints added

---
+++ VeterinarySurgeon.Web/Endpoints/Animal/GetById.AnimalResponse.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using VeterinarySurgeon.Application.Services;
+
+namespace VeterinarySurgeon.Web.Endpoints.Animal
+{
+    public class AnimalResponse
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        // Mappers
+        // Note: doesn't expose behavior
+        public static AnimalResponse FromAnimal(AnimalDTO item) =>
+            new AnimalResponse()
+            {
+                Id = item.Id,
+                Name = item.Name
+            };
+
+        public static AnimalDTO FromAnimalDTO(AnimalResponse item) => new AnimalDTO()
+        {
+            Id = item.Id,
+            Name = item.Name
+        };
+    }
+}
--- VeterinarySurgeon.Web/Endpoints/Animal/GetById.cs
+++ VeterinarySurgeon.Web/Endpoints/Animal/GetById.cs
@@ -1,35 +1,6 @@
-﻿using Ardalis.ApiEndpoints;
-using Microsoft.AspNetCore.Mvc;
-using Swashbuckle.AspNetCore.Annotations;
-using System.Threading.Tasks;
-
-namespace VeterinarySurgeon.Web.Endpoints.Animal
+﻿namespace VeterinarySurgeon.Web.Endpoints.Animal
 {
-    //public class GetById : BaseAsyncEndpoint<int, AnimalResponse>
-    //{
-    //    //private readonly IRepository _repository;
-
-    //    //public GetById(IRepository repository)
-    //    //{
-    //    //    _repository = repository;
-    //    //}
-
-    //    //[HttpGet("/animals/{id:int}")]
-    //    //[SwaggerOperation(
-    //    //    Summary = "Gets a single Animal",
-    //    //    Description = "Gets a single Animal by Id",
-    //    //    OperationId = "Animal.GetById",
-    //    //    Tags = new[] { "AnimalEndpoints" })
-    //    //]
-    //    //public override async Task<ActionResult<AnimalResponse>> HandleAsync(int id)
-    //    //{
-    //    //    var item = await _repository.GetByIdAsync<Core.Entities.Animal>(id);
-
-    //    //    var response = new AnimalResponse
-    //    //    {
-    //    //        Name = item.Name
-    //    //    };
-    //    //    return Ok(response);
-    //    //}
-    //}
+    public class GetById
+    {
+    }
 }
